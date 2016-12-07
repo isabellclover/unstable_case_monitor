c52838eb1bd0bc4498e7ed458c45a14a1644c89b
Isabel Bai <isabel_bai@symantec.com>
2016-12-06 16:53:39 -0800

    update path

--- scripts/get_artifacts.groovy
+++ scripts/get_artifacts.groovy
@@ -1,6 +1,7 @@
 // copy the output from all the triggered jobs by invoking the Copy Artifact plugin for the  
 // builds that were triggered  
 import hudson.plugins.copyartifact.*
+import org.jenkinsci.plugins.testresultanalyzer.*
 import hudson.model.AbstractBuild
 import hudson.Launcher
 import hudson.model.BuildListener
@@ -8,6 +9,8 @@ import hudson.FilePath
 
 def builders = [88,89,90]
 def jobname = "clean-sweep_develop_tests_local"
+def reportPath = "TestReports"
+
 for (subBuild in builders) {
   println(jobname + " => " + subBuild)
   copyTriggeredResults(jobname, Integer.toString(subBuild))
@@ -15,10 +18,11 @@ for (subBuild in builders) {
 
 def copyTriggeredResults(projName, buildNumber) {
    def selector = new SpecificBuildSelector(buildNumber)
+   def targetPath = reportPath+ "/"+ projName + "/"+ buildNumber + "/**";
 
    // CopyArtifact(String projectName, String parameters, BuildSelector selector,
    // String filter, String target, boolean flatten, boolean optional)
-   def copyArtifact = new CopyArtifact(projName, "", selector, "**", null, false, true)
+   def copyArtifact = new CopyArtifact(projName, "", selector, targetPath, null, false, true)
 
    // use reflection because direct call invokes deprecated method
    // perform(Build<?, ?> build, Launcher launcher, BuildListener listener)
