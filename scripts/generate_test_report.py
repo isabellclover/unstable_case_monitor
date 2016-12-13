285bb0ccc947706ebd665f798bf147c7babe18d3
Isabel Bai <isabel_bai@symantec.com>
2016-12-12 17:02:15 -0800

    add more data in report

--- scripts/generate_test_report.py
+++ scripts/generate_test_report.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import os
+from html_helper import *
 
 SUMMARY_TEXT = 'summary_text'
 FINAL_REPORT = 'final_report.html'
@@ -10,6 +11,7 @@ PRODUCT_NAME = 'Norton Clean'
 CURRENT = os.path.dirname(os.path.abspath(__file__))
 TEMP_PATH = os.path.join(CURRENT, 'Temp')
 BUILD_STAMP = 'BUILD_STAMP'
+JOB_PATH_NAME = 'http://nortonmobile.usccqa.qalabs.symantec.com/jenkins/job/'
 
 def grab_unstable_case_list(root_path, target_file_path):    
     total = 0
@@ -21,6 +23,7 @@ def grab_unstable_case_list(root_path, target_file_path):
             if '.xml' in filename and 'TEST' in filename:
                 print 'Find Test report as' + filename
                 print root
+                
                 #get job and build information
                 jobinfo = root[root.find('TestReports'):].split(os.sep)
                 jobname = jobinfo[1]
@@ -42,57 +45,19 @@ def grab_unstable_case_list(root_path, target_file_path):
                 if general.attrib['failures']=='0' and general.attrib['errors']=='0':
                     print 'all passed, skip'
                     continue
+                
                 #find error/failed case
                 else:
                     failure += int(general.attrib['failures'])
                     error += int(general.attrib['errors'])                    
                     for failcase in general.findall('.//failure/..'):
-                        print 'fail case:' + failcase.tag
                         target_file.write(str(failcase.attrib['classname']+'.'+failcase.attrib['name']) + os.linesep)
                     for errorcase in general.findall(".//error/.."):
-                        print 'error case:' + errorcase.tag
                         target_file.write(str(failcase.attrib['classname']+'.'+failcase.attrib['name']) + os.linesep)
+
+                #write summary
                 target_file.write('\n'+SUMMARY_TEXT+'Total:'+str(total)+',Failure:'+str(failure)+',Error:'+str(error))
                 target_file.close()    
-
-def build_html_header():
-    html_txt = '<!DOCTYPE html><html><head>'
-    html_txt += '</head><body>'
-    return html_txt
-
-def build_html_foot():
-    html_txt = '</body></html>'
-    return html_txt
-
-def build_table_header(cols):
-    html_txt = '<table style="border: 1px solid #000000;text-align: left;padding: 8px;"><tr style="background-color:#dddddd">'
-    for col in cols:
-        html_txt += ('<th>'+str(col)+'</th>')
-    html_txt += '</tr>'
-    return html_txt
-
-def build_table_foot():
-    return '</table>'
-
-def build_case_row(datas):
-    html = '<tr>'
-    for data in datas:
-        html += '<td style="text-align:left">'+str(data)+'</td>'
-    html += '</tr>'
-    return html
-
-def build_bond_text(line):
-    html = '<p><b>'+line+'</b></p>'
-    return html
-
-def build_normal_text(line):
-    return '<p>'+line+'</p>'
-
-def build_title():
-    html = '<h1>'
-    html += (PRODUCT_NAME+' '+BRANCH_NAME+' '+'Unstable Case')
-    html += '</h1>'
-    return html
 
 def write_report(source, dest):
     # filter data
@@ -101,38 +66,64 @@ def write_report(source, dest):
     # create html report
     dest_file = open(dest, 'w')    
     dest_file.write(build_html_header())
-    dest_file.write(build_title())
+    dest_file.write(build_title(PRODUCT_NAME, BRANCH_NAME))
     
     # inject tables
     for root,directories,files in os.walk(TEMP_PATH):
+        # one file for one job
         for filename in files:                              
-            buildnumber = 0
+            total_builds = 0
+            max_build = 0        
             source_file=open(os.path.join(root,filename), 'r')
-            result={}           
+            #final case map
+            result={}            
 
             #write case table
-            for casename in source_file:
+            for casename in source_file:                
                 if BUILD_STAMP in casename:
-                    buildnumber += 1
+                    total_builds += 1
+                    current = int(casename[casename.find(BUILD_STAMP)+11:])
+                    if current >= max_build:
+                        max_build = current
                 elif SUMMARY_TEXT in casename:
                     #dest_file.write(casename.replace(SUMMARY_TEXT,''))
                     continue
                 else:
+                    #single case_info [fail_time, max_failed, is_still_failing]
                     if casename in result.keys():
-                        result[casename]+=1
+                        case_info = result[casename]
+                        case_info[0] += 1
+                        case_info[1] = max_build
                     elif len(casename) >= 2:
-                        result[casename]=1
+                        case_info = [1, max_build, False]                    
+                        result[casename] = case_info   
             source_file.close()
             
             #write job name
-            dest_file.write(build_bond_text('Job['+ filename+ '] Latest [' + str(buildnumber) +']Builds'))
+            dest_file.write(build_bond_text('Job['+ filename+ '] Latest [' + str(total_builds) +']Builds'))
+            dest_file.write(build_normal_text(build_link_text('Job Test Analysis Report', JOB_PATH_NAME+filename)))
+            
+            #no error case
             if len(result) == 0:
+                dest_file.write(build_normal_text('Stable. No Error Case Recently.'))
                 continue
-            #cols = ['Package Name','Case Name', 'Failing Rate', 'Still Fail in latest 5 builds', 'Latest Message', 'Report Link']
-            cols = ['Case Name','Total Builds', 'Failed Builds', 'Failing Rate']
+            
+            #write table cols
+            cols = ['Case Name','Total Builds', 'Failed Builds', 'Failing(%)','Fail in Latest Build']
             dest_file.write(build_table_header(cols))
+
+            #write table data
             for key in result.keys():
-                dest_file.write(build_case_row([key,buildnumber, result[key], str(result[key]/buildnumber*100)+'%']))
+                case_info = result[key]
+                if max_build <= case_info[1]:
+                    case_info[2] = True
+                failing_rate = int(case_info[0])*100/total_builds
+                latest_failing = 'Unknown'
+                if(case_info[2]):
+                    latest_failing = 'Yes / Build '+str(case_info[1])
+                else:
+                    latest_failing = 'No'
+                dest_file.write(build_case_row([key, total_builds, case_info[0], str(failing_rate)+'%', latest_failing]))
             dest_file.write(build_table_foot())
 
     # end the html        
---
+++ scripts/html_helper.py
@@ -0,0 +1,47 @@
+import xml.etree.ElementTree as ET
+import os
+
+def build_html_header():
+    html_txt = '<!DOCTYPE html><html><head>'
+    html_txt += '</head><body>'
+    return html_txt
+
+def build_html_foot():
+    html_txt = '</body></html>'
+    return html_txt
+
+def build_table_header(cols):
+    html_txt = '<table style="border: 1px solid #000000;text-align: left;padding: 8px;"><tr style="background-color:#dddddd">'
+    for col in cols:
+        html_txt += ('<th>'+str(col)+'</th>')
+    html_txt += '</tr>'
+    return html_txt
+
+def build_table_foot():
+    return '</table>'
+
+def build_case_row(datas):
+    html = '<tr>'
+    for data in datas:
+        html += '<td style="text-align:left">'+str(data)+'</td>'
+    html += '</tr>'
+    return html
+
+def build_bond_text(line):
+    html = '<p><b>'+ str(line) +'</b></p>'
+    return html
+
+def build_normal_text(line):
+    return '<p>'+ str(line)+'</p>'
+
+def build_title(product, branch):
+    html = '<h1>'
+    html += (product+' '+branch+' '+'Unstable Case')
+    html += '</h1>'
+    return html
+
+def build_link_text(link_text, url):
+    html = '<u><a href=\"'
+    html += url
+    html += ('\">' + link_text + '</a></u>')
+    return html 
