a4d5dbe141da5d283725b9c2859d286885d9f26d
Isabel Bai <isabel_bai@symantec.com>
2016-12-12 17:23:03 -0800

    update style

--- scripts/html_helper.py
+++ scripts/html_helper.py
@@ -3,7 +3,6 @@ import os
 
 def build_html_header():
     html_txt = '<!DOCTYPE html><html><head>'
-    html_txt += '<style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style>'
     html_txt += '</head><body>'
     return html_txt
 
@@ -12,7 +11,7 @@ def build_html_foot():
     return html_txt
 
 def build_table_header(cols):
-    html_txt = '<table style="border: 1px solid black;text-align: left;padding: 8px;"><tr style="background-color:#dddddd">'
+    html_txt = '<table class="pane sortable bigtable stripped"><tr class="pane-header">'
     for col in cols:
         html_txt += ('<th>'+str(col)+'</th>')
     html_txt += '</tr>'
@@ -24,7 +23,7 @@ def build_table_foot():
 def build_case_row(datas):
     html = '<tr>'
     for data in datas:
-        html += '<td style="text-align:left">'+str(data)+'</td>'
+        html += '<td class="pane no-wrap">'+str(data)+'</td>'
     html += '</tr>'
     return html
 
